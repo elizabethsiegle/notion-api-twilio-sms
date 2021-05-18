7caacf7b21582c753dc250a7a62268048e630af9
Lizzie Siegle <esiegle@brynmawr.edu>
2021-05-17 18:03:53 -0700

    add 1 item via sms

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -1,13 +1,20 @@
 const superagent = require('superagent');
+const fetch = require('node-fetch');
 exports.handler = async function(context, event, callback) {
   const twiml = new Twilio.twiml.MessagingResponse();
   let inbMsg = event.Body.trim();
-  superagent.get(
-    `https://api.notion.com/v1/databases/920339cdab384bb29f087c10336cce32/`)
-    .set('Authorization', `Bearer ${context.NOTION_API_KEY}`)
-    .end((err, res) => {
-      twiml.message(res.text);
-      callback(null, twiml);
-    })
-  
+  superagent.post(`https://api.notion.com/v1/pages`, 
+  { "parent": { 
+    "database_id": `${context.NOTION_DB_ID}`
+  }, "properties": 
+  { "Name": 
+  { "title": 
+  [ { "text": { "content": `${inbMsg}` } } ] } } })
+  .set('Authorization', `Bearer ${context.NOTION_API_KEY}`)
+  .set('Content-Type', 'application/json')
+  .set('Notion-Version', '2021-05-13')
+  .end((err, res) => {
+    twiml.message(`posted ${inbMsg} to the database!`);
+    callback(null, twiml);
+  });
 };
--- functions/retrieve-notion-db.js
+++ functions/retrieve-notion-db.js
@@ -2,8 +2,7 @@ const superagent = require('superagent');
 exports.handler = async function(context, event, callback) {
   const twiml = new Twilio.twiml.MessagingResponse();
   let inbMsg = event.Body.trim();
-  superagent.get(
-    `https://api.notion.com/v1/databases/920339cdab384bb29f087c10336cce32/`)
+  superagent.get(`https://api.notion.com/v1/databases/920339cdab384bb29f087c10336cce32/`)
     .set('Authorization', `Bearer ${context.NOTION_API_KEY}`)
     .end((err, res) => {
       twiml.message(res.text);
